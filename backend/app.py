9d6f79a7e2e02b7798ec7afad1f902df38806161
kali204 <abhaystark77@gmail.com>
2025-07-23 22:54:38 +0530

    Initial commit of full-stack chat application

--- backend/app.py
+++ backend/app.py
@@ -25,7 +25,7 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 db = SQLAlchemy(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
-CORS(app, resources={r"/api/*": {"origins": "https://chatting-nine-eta.vercel.app/"}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "https://chatting-nine-eta.vercel.app"}}, supports_credentials=True)
 
 @app.route('/')
 def home():
